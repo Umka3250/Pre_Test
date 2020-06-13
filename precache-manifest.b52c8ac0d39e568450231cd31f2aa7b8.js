07d2ff64a0393a8daf842f33910bd15a28c601a3
Umka3250 <joker1233214@gmail.com>
2020-06-13 23:31:41 +0300

    Updates

---
+++ precache-manifest.b52c8ac0d39e568450231cd31f2aa7b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9affb3f86287f67d22fa0c139530ac7c",
+    "url": "/Pre_Test/index.html"
+  },
+  {
+    "revision": "f205aaade71c9c046363",
+    "url": "/Pre_Test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c5fda06ce90875459e9b",
+    "url": "/Pre_Test/static/css/main.7f0b8fc5.chunk.css"
+  },
+  {
+    "revision": "f205aaade71c9c046363",
+    "url": "/Pre_Test/static/js/2.4102057b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Pre_Test/static/js/2.4102057b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fda06ce90875459e9b",
+    "url": "/Pre_Test/static/js/main.c501b13a.chunk.js"
+  },
+  {
+    "revision": "bebc3b835fee6bbbd993",
+    "url": "/Pre_Test/static/js/runtime-main.1ed028cd.js"
+  }
+]);
